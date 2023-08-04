5891177afac4acf7b7b3d83c3c9b55427436d441
codev009 <lipin007@gmail.com>
2023-08-04 06:45:24 +0530

    basic setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,23 @@
 <script>
-	let firstName = 'Lipin';
-  let lastName = 'Pappachan';
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: "lorem ipsum",
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: "lorem ipsum",
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: "lorem ipsum",
+    },
+  ];
 </script>
 
 <main>
-  <h1>Hello {firstName} {lastName}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello</h1>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
